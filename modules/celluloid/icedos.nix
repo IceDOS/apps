49fa344fa7ff040d1e8f8326a0c96c0b29c9cf7b
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-09 00:03:16 +0300

    feat(celluloid): add module

---
+++ modules/celluloid/icedos.nix
@@ -0,0 +1,45 @@
+{ ... }:
+
+{
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          pkgs,
+          lib,
+          config,
+          ...
+        }:
+        {
+          environment.systemPackages = with pkgs; [
+            (writeShellScriptBin "celluloid-hdr" "ENABLE_HDR_WSI=1 celluloid --mpv-profile=HDR $@")
+            celluloid
+          ];
+
+          home-manager.users = lib.mapAttrs (user: _: {
+            home.file.".config/celluloid" = {
+              source = ./config;
+              recursive = true;
+            };
+
+            dconf.settings = {
+              "io/github/celluloid-player/celluloid" = {
+                mpv-config-file = "file:///home/${user}/.config/celluloid/celluloid.conf";
+              };
+
+              "io/github/celluloid-player/celluloid" = {
+                mpv-config-enable = true;
+              };
+
+              "io/github/celluloid-player/celluloid" = {
+                always-append-to-playlist = true;
+              };
+            };
+          }) config.icedos.system.users;
+        }
+      )
+    ];
+
+  meta.name = "celluloid";
+}
