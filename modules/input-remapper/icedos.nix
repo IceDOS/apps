3fdac92a8bdecda87ccb6009865022a9a213d1e8
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-09 01:46:08 +0300

    feat(input-remapper): add module

---
+++ modules/input-remapper/icedos.nix
@@ -0,0 +1,43 @@
+{ ... }:
+
+{
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          pkgs,
+          lib,
+          config,
+          ...
+        }:
+        let
+          inherit (lib) mapAttrs;
+          users = config.icedos.system.users;
+        in
+        {
+          services.input-remapper.enable = true;
+
+          home-manager.users = mapAttrs (user: _: {
+            systemd.user.services.input-remapper-autoload-fix = {
+              Unit.Description = "Input Remapper Autoload Fix";
+              Install.WantedBy = [ "graphical-session.target" ];
+
+              Service = {
+                ExecStart = "${pkgs.input-remapper}/bin/input-remapper-control --command autoload";
+                Nice = "-20";
+                Restart = "on-failure";
+                StartLimitIntervalSec = 60;
+                StartLimitBurst = 60;
+              };
+            };
+
+          }) users;
+
+          users.users = mapAttrs (user: _: { extraGroups = [ "input" ]; }) users;
+        }
+      )
+    ];
+
+  meta.name = "input-remapper";
+}
