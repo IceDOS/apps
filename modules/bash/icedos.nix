e644a15a1e7572ee41fb54b517784abee606306e
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-10 20:18:57 +0300

    feat(modules): add bash

---
+++ modules/bash/icedos.nix
@@ -0,0 +1,29 @@
+{ ... }:
+
+{
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          config,
+          lib,
+          ...
+        }:
+
+        let
+          inherit (lib) mapAttrs;
+          cfg = config.icedos;
+        in
+        {
+          home-manager.users = mapAttrs (user: _: {
+            home.file.".bashrc".text = ""; # Avoid file not found errors
+          }) cfg.system.users;
+
+          security.sudo.extraConfig = "Defaults pwfeedback"; # Show asterisks when typing sudo password
+        }
+      )
+    ];
+
+  meta.name = "bash";
+}
