8fa611aff0aa75952bd66c23057baf7909f2b3db
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-11 01:49:32 +0300

    feat(modules): add zenstates

---
+++ modules/zenstates/icedos.nix
@@ -0,0 +1,45 @@
+{ icedosLib, ... }:
+
+{
+  options.icedos.applications.zenstates.serviceArgs = icedosLib.mkStrOption { default = ""; };
+
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          config,
+          pkgs,
+          ...
+        }:
+
+        {
+          environment.systemPackages = [
+            pkgs.zenstates
+          ];
+
+          systemd.services.zenstates = {
+            enable = true;
+            description = "Ryzen Undervolt";
+            after = [
+              "syslog.target"
+              "systemd-modules-load.service"
+            ];
+
+            unitConfig = {
+              ConditionPathExists = "${pkgs.zenstates}/bin/zenstates";
+            };
+
+            serviceConfig = {
+              User = "root";
+              ExecStart = "${pkgs.zenstates}/bin/zenstates ${config.icedos.applications.zenstates.serviceArgs}";
+            };
+
+            wantedBy = [ "multi-user.target" ];
+          };
+        }
+      )
+    ];
+
+  meta.name = "zenergy";
+}
