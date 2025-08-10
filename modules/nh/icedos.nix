77d39a77eef09aa12ec814927bf24b4ba0138cf0
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-10 20:27:07 +0300

    feat(modules): add nh

---
+++ modules/nh/icedos.nix
@@ -0,0 +1,55 @@
+{ icedosLib, lib, ... }:
+
+{
+  options.icedos.applications.nh.gc =
+    let
+      inherit (icedosLib) mkBoolOption mkNumberOption mkStrOption;
+      gc = (fromTOML (lib.fileContents ./config.toml)).icedos.applications.nh.gc;
+    in
+    {
+      automatic = mkBoolOption { default = gc.automatic; };
+      days = mkNumberOption { default = gc.days; };
+      generations = mkNumberOption { default = gc.generations; };
+      interval = mkStrOption { default = gc.interval; };
+    };
+
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          config,
+          pkgs,
+          ...
+        }:
+
+        let
+          cfg = config.icedos.applications.nh.gc;
+          command = "gc";
+          days = "${toString (cfg.days)}d";
+          generations = toString (cfg.generations);
+        in
+        {
+          icedos.internals.toolset.commands = [
+            {
+              bin = "${pkgs.writeShellScript command ''"${pkgs.nh}/bin/nh" clean all -k "${generations}" -K "${days}"''}";
+              command = command;
+              help = "clean nix plus home manager, store and profiles";
+            }
+          ];
+
+          programs.nh = {
+            enable = true;
+
+            clean = {
+              enable = cfg.automatic;
+              extraArgs = "-k ${toString (cfg.generations)} -K ${days}";
+              dates = cfg.interval;
+            };
+          };
+        }
+      )
+    ];
+
+  meta.name = "nh";
+}
