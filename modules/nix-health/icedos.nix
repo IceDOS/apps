ce9d6300eaca19b328d134f7de0e98dcb897fc96
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-10 18:50:48 +0300

    feat(modules): add nix-health

---
+++ modules/nix-health/icedos.nix
@@ -0,0 +1,32 @@
+{ ... }:
+
+{
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          pkgs,
+          ...
+        }:
+
+        {
+          icedos.internals.toolset.commands = [
+            (
+              let
+                command = "health";
+              in
+              {
+                bin = "${pkgs.writeShellScript command ''"${pkgs.nix-health}/bin/nix-health" -q "$@"''}";
+
+                command = command;
+                help = "print information about system state";
+              }
+            )
+          ];
+        }
+      )
+    ];
+
+  meta.name = "nix-health";
+}
