abc2a45797c050b66c33d51edd34e65559826199
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-10 20:10:55 +0300

    feat(modules): add btop

---
+++ modules/btop/icedos.nix
@@ -0,0 +1,30 @@
+{ ... }:
+
+{
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          config,
+          pkgs,
+          lib,
+          ...
+        }:
+
+        let
+          inherit (lib) mapAttrs;
+          cfg = config.icedos;
+        in
+        {
+          environment.systemPackages = [ pkgs.btop ];
+
+          home-manager.users = mapAttrs (user: _: {
+            home.file.".config/btop/btop.conf".source = ./btop.conf;
+          }) cfg.system.users;
+        }
+      )
+    ];
+
+  meta.name = "btop";
+}
