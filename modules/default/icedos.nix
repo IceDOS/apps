d2f08c1230a5e12fa5e32122b47fef5d352e90ab
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-10 22:12:03 +0300

    feat(modules): add default module

---
+++ modules/default/icedos.nix
@@ -0,0 +1,56 @@
+{ icedosLib, ... }:
+
+{
+  options.icedos.applications =
+    let
+      inherit (icedosLib) mkStrListOption;
+    in
+    {
+      extraPackages = mkStrListOption { default = [ ]; };
+      insecurePackages = mkStrListOption { default = [ ]; };
+    };
+
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+
+        let
+          inherit (lib)
+            foldl'
+            lists
+            splitString
+            ;
+
+          cfg = config.icedos;
+
+          pkgMapper =
+            pkgList: lists.map (pkgName: foldl' (acc: cur: acc.${cur}) pkgs (splitString "." pkgName)) pkgList;
+        in
+        {
+          environment.systemPackages =
+            with pkgs;
+            [
+              efibootmgr # Edit EFI entries
+              killall # Tool to kill all programs matching process name
+              ntfs3g # Support NTFS drives
+              p7zip # 7zip
+              unrar # Support opening rar files
+              unzip # An extraction utility
+              wget # Terminal downloader
+            ]
+            ++ (pkgMapper cfg.applications.extraPackages);
+
+          nixpkgs.config.permittedInsecurePackages = cfg.applications.insecurePackages;
+        }
+      )
+    ];
+
+  meta.name = "default";
+}
