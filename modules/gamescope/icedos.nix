94724694ad943247a6e6a94efa6c2e6c278aa5ff
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-10 16:56:47 +0300

    feat(modules): add gamescope

---
+++ modules/gamescope/icedos.nix
@@ -0,0 +1,43 @@
+{ icedosLib, ... }:
+
+{
+  options.icedos.applications.gamescope = icedosLib.mkBoolOption { default = true; };
+
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+
+        let
+          inherit (lib)
+            mapAttrs
+            mkIf
+            ;
+
+          cfg = config.icedos;
+          package = [ pkgs.gamescope ];
+          ifSteam = deck: cfg.applications.steam.enable && deck;
+        in
+        {
+          environment.systemPackages = package;
+          programs.steam.extraPackages = mkIf (ifSteam (cfg.hardware.devices.steamdeck)) package;
+
+          home-manager.users = mapAttrs (user: _: {
+            home.packages =
+              mkIf (ifSteam (!cfg.hardware.devices.steamdeck) && !cfg.applications.proton-launch)
+                [
+                  (pkgs.steam.override { extraPkgs = pkgs: package; })
+                ];
+          }) cfg.system.users;
+        }
+      )
+    ];
+
+  meta.name = "gamescope";
+}
