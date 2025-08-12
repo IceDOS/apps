4bb499a84343aa8a8cc3287d7945edb24b3cd87a
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-12 20:17:47 +0300

    feat(modules): add sd-inhibitor

---
+++ modules/sd-inhibitor/icedos.nix
@@ -0,0 +1,102 @@
+{ icedosLib, ... }:
+
+{
+  options.icedos.system.users =
+    let
+      inherit (icedosLib)
+        mkBoolOption
+        mkNumberOption
+        mkStrListOption
+        mkSubmoduleAttrsOption
+        ;
+    in
+    mkSubmoduleAttrsOption { default = { }; } {
+      applications.sd-inhibitor.watchers = {
+        cpu = {
+          enable = mkBoolOption { default = false; };
+          threshold = mkNumberOption { default = 60; };
+        };
+
+        disk = {
+          enable = mkBoolOption { default = false; };
+          threshold = mkNumberOption { default = 10; };
+        };
+
+        network = {
+          enable = mkBoolOption { default = false; };
+          threshold = mkNumberOption { default = 1000000; };
+        };
+
+        pipewire = {
+          enable = mkBoolOption { default = false; };
+          inputsToIgnore = mkStrListOption { default = [ ]; };
+          outputsToIgnore = mkStrListOption { default = [ ]; };
+        };
+      };
+    };
+
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+
+        let
+          inherit (lib)
+            attrNames
+            filterAttrs
+            mapAttrs
+            mkIf
+            optional
+            readFile
+            ;
+
+          cfg = config.icedos;
+
+          getModules =
+            path:
+            map (dir: ./. + ("/modules/" + dir)) (
+              attrNames (filterAttrs (_: v: v == "directory") (builtins.readDir path))
+            );
+        in
+        {
+          imports = getModules ./modules;
+
+          home-manager.users = mapAttrs (user: _: {
+            systemd.user.services.sd-inhibitor =
+              let
+                watchers = cfg.system.users.${user}.applications.sd-inhibitor.watchers;
+              in
+              mkIf
+                (watchers.cpu.enable || watchers.disk.enable || watchers.network.enable || watchers.pipewire.enable)
+                {
+                  Unit = {
+                    Description = "service to inhibit idle, sleep and shutdown based on device usage limits";
+                    After = "graphical-session.target";
+                    PartOf = "graphical-session.target";
+                  };
+
+                  Install.WantedBy =
+                    [ ]
+                    ++ optional (cfg.desktop.hyprland.enable) "hyprland-session.target"
+                    ++ optional (cfg.desktop.gnome.enable) "gnome-session.target";
+
+                  Service = {
+                    ExecStart = with pkgs; "${writeShellScript "sd-inhibitor" (readFile ./sd-inhibitor.sh)}";
+                    Nice = "-20";
+                    Restart = "on-failure";
+                    StartLimitBurst = 60;
+                  };
+                };
+          }) cfg.system.users;
+        }
+      )
+    ];
+
+  meta.name = "sd-inhibitor";
+}
---
+++ modules/sd-inhibitor/modules/cpu-watcher/default.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  inherit (lib) mapAttrs mkIf;
+  cfg = config.icedos;
+in
+{
+  home-manager.users = mapAttrs (user: _: {
+    home.packages =
+      let
+        watcher = cfg.system.users.${user}.applications.sd-inhibitor.watchers.cpu;
+      in
+      mkIf (watcher.enable) [
+        (pkgs.writeShellScriptBin "cpu-watcher" ''
+          UTILIZATION="$((100-$(vmstat 1 2|tail -1|awk '{print $15}')))"
+          CPU_THRESOLD=${toString (watcher.threshold)}
+
+          if (( UTILIZATION > CPU_THRESOLD )) then
+              printf true
+          else
+              printf false
+          fi
+        '')
+      ];
+  }) cfg.system.users;
+}
