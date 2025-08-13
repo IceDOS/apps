1d7e3e9a9b731b4c6e2647b27092c6fcf07d33ef
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-13 06:48:49 +0300

    feat(steam): add session module

---
+++ modules/steam/modules/session/icedos.nix
@@ -0,0 +1,49 @@
+{ icedosLib, ... }:
+
+{
+  options.icedos.applications.steam.session =
+    let
+      inherit (icedosLib) mkBoolOption mkStrOption;
+    in
+    {
+      autoStart = {
+        enable = mkBoolOption { default = false; };
+        desktopSession = mkStrOption { default = ""; };
+      };
+
+      user = mkStrOption { default = ""; };
+    };
+
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          config,
+          lib,
+          ...
+        }:
+
+        let
+          inherit (lib) hasAttr;
+          cfg = config.icedos;
+          session = cfg.applications.steam.session;
+        in
+        {
+          jovian = {
+            hardware.has.amd.gpu = hasAttr "radeon" cfg.hardware.graphics;
+
+            steam = {
+              enable = true;
+              autoStart = session.autoStart.enable;
+              desktopSession = session.autoStart.desktopSession;
+              updater.splash = if (hasAttr "steamdeck" cfg.hardware.devices) then "jovian" else "vendor";
+              user = session.user;
+            };
+          };
+        }
+      )
+    ];
+
+  meta.name = "steam-session";
+}
