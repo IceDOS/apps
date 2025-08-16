9951c752a4a7b00e4d5ff9005fa3653ce6754770
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-16 19:38:42 +0300

    feat(nh): add clean script for icedos builds

--- modules/nh/icedos.nix
+++ modules/nh/icedos.nix
@@ -28,11 +28,47 @@
           command = "gc";
           days = "${toString (cfg.days)}d";
           generations = toString (cfg.generations);
+
+          cleanExtra =
+            let
+              bc = "${pkgs.bc}/bin/bc";
+              command = "nh-clean-extra";
+            in
+            "${pkgs.writeShellScriptBin command ''
+              BLUE='\e[34m'
+              GREEN='\033[0;32m'
+              RED='\033[0;31m'
+              NC='\033[0m'
+
+
+              echo -e "\n''${BLUE}/tmp/nix-shell-*/icedos-build''${NC}"
+
+              tempBuildDirs=$(find /tmp -type d -name "icedos-build*" 2>/dev/null)
+              totalSize=0
+
+              for dir in $tempBuildDirs; do
+                  echo -e "- ''${RED}DEL''${NC} $dir"
+                  sizeKb=$(du -sk "$dir" | cut -f1)
+                  sizeMb=$(echo "scale=2; $sizeKb / 1024" | ${bc})
+                  totalSize=$(echo "scale=2; $totalSize + $sizeMb" | ${bc})
+              done
+
+              formattedTotal=$(printf "%.2f" "$totalSize")
+
+              echo -e
+
+              for dir in $tempBuildDirs; do
+                  echo -e "''${GREEN}>''${NC} Removing $dir"
+                  rm -r "$dir"
+              done
+
+              echo -e "\n''${formattedTotal} MiB freed"
+            ''}/bin/${command}";
         in
         {
           icedos.applications.toolset.commands = [
             {
-              bin = "${pkgs.writeShellScript command ''"${pkgs.nh}/bin/nh" clean all -k "${generations}" -K "${days}"''}";
+              bin = "${pkgs.writeShellScript command ''"${pkgs.nh}/bin/nh" clean all -k "${generations}" -K "${days}" && ${cleanExtra}''}";
               command = command;
               help = "clean nix plus home manager, store and profiles";
             }
@@ -47,6 +83,8 @@
               dates = cfg.interval;
             };
           };
+
+          systemd.services.nh-clean.serviceConfig.ExecStartPost = lib.mkIf cfg.automatic cleanExtra;
         }
       )
     ];
