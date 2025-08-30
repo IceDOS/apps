9f6ad1dc348c6c1a2abe014e1395f2f727e08326
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-30 19:06:33 +0300

    feat(wolf): add app state folder option

--- modules/wolf/icedos.nix
+++ modules/wolf/icedos.nix
@@ -1,42 +1,51 @@
-{ ... }:
+{ icedosLib, ... }:
 
 {
+  options.icedos.applications.wolf.stateFolder = icedosLib.mkStrOption { default = "/etc/wolf"; };
+
   outputs.nixosModules =
     { ... }:
     [
-      {
-        virtualisation = {
-          oci-containers = {
-            backend = "docker";
+      (
+        { config, ... }:
+        {
+          virtualisation = {
+            oci-containers =
+              let
+                stateFolder = config.icedos.applications.wolf.stateFolder;
+              in
+              {
+                backend = "docker";
 
-            containers.wolf = {
-              image = "ghcr.io/games-on-whales/wolf:stable";
+                containers.wolf = {
+                  image = "ghcr.io/games-on-whales/wolf:stable";
 
-              extraOptions = [
-                "--pull=always"
-                "--device-cgroup-rule=c 13:* rmw"
-                "--network=host"
-                "--device=/dev/dri"
-                "--device=/dev/uinput"
-                "--device=/dev/uhid"
-              ];
+                  extraOptions = [
+                    "--pull=always"
+                    "--device-cgroup-rule=c 13:* rmw"
+                    "--network=host"
+                    "--device=/dev/dri"
+                    "--device=/dev/uinput"
+                    "--device=/dev/uhid"
+                  ];
 
-              environment = {
-                XDG_RUNTIME_DIR = "/tmp/sockets";
-                HOST_APPS_STATE_FOLDER = "/etc/wolf";
+                  environment = {
+                    XDG_RUNTIME_DIR = "/tmp/sockets";
+                    HOST_APPS_STATE_FOLDER = stateFolder;
+                  };
+
+                  volumes = [
+                    "${stateFolder}:${stateFolder}"
+                    "/tmp/sockets:/tmp/sockets:rw"
+                    "/var/run/docker.sock:/var/run/docker.sock:rw"
+                    "/dev/:/dev/:rw"
+                    "/run/udev:/run/udev:rw"
+                  ];
+                };
               };
-
-              volumes = [
-                "/etc/wolf/:/etc/wolf"
-                "/tmp/sockets:/tmp/sockets:rw"
-                "/var/run/docker.sock:/var/run/docker.sock:rw"
-                "/dev/:/dev/:rw"
-                "/run/udev:/run/udev:rw"
-              ];
-            };
           };
-        };
-      }
+        }
+      )
     ];
 
   meta.name = "wolf";
