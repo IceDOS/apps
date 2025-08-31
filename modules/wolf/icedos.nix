f42c91a1cc98908d38ab347feeec7ca2a9ef613b
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-31 16:31:23 +0300

    feat(wolf): passthrough nix apps, gpu drivers and add extra volumes support

--- modules/wolf/icedos.nix
+++ modules/wolf/icedos.nix
@@ -1,18 +1,39 @@
 { icedosLib, ... }:
 
+let
+  defaultStateFolder = "/etc/wolf";
+
+  defaultVolumes = [
+    "/nix/store:/nix/store:ro"
+    "/run/current-system/sw/bin:/host-apps:ro"
+    "/run/opengl-driver:/run/opengl-driver:ro"
+  ];
+in
 {
-  options.icedos.applications.wolf.stateFolder = icedosLib.mkStrOption { default = "/etc/wolf"; };
+  options.icedos.applications.wolf = {
+    extraVolumes = icedosLib.mkStrListOption { default = [ ]; };
+    stateFolder = icedosLib.mkStrOption { default = defaultStateFolder; };
+  };
 
   outputs.nixosModules =
     { ... }:
     [
       (
-        { config, ... }:
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        let
+          wolf = config.icedos.applications.wolf;
+        in
         {
           virtualisation = {
             oci-containers =
               let
-                stateFolder = config.icedos.applications.wolf.stateFolder;
+                inherit (lib) optional;
+                inherit (wolf) stateFolder;
               in
               {
                 backend = "docker";
@@ -35,15 +56,48 @@
                   };
 
                   volumes = [
-                    "${stateFolder}:${stateFolder}"
-                    "/tmp/sockets:/tmp/sockets:rw"
-                    "/var/run/docker.sock:/var/run/docker.sock:rw"
-                    "/dev/:/dev/:rw"
-                    "/run/udev:/run/udev:rw"
-                  ];
+                    "/dev:/dev"
+                    "/etc/wolf:/etc/wolf"
+                    "/run/udev:/run/udev"
+                    "/tmp/sockets:/tmp/sockets"
+                    "/var/run/docker.sock:/var/run/docker.sock"
+                  ]
+                  ++ optional (stateFolder != defaultStateFolder) "${stateFolder}:${stateFolder}";
                 };
               };
           };
+
+          systemd.services.docker-wolf.preStart =
+            let
+              inherit (builtins) toJSON;
+              inherit (lib) concatStringsSep;
+              inherit (pkgs) jq toml-cli;
+              inherit (wolf) extraVolumes;
+
+              jqBin = "${jq}/bin/jq";
+              tomlBin = "${toml-cli}/bin/toml";
+              volumes = extraVolumes ++ defaultVolumes;
+            in
+            ''
+              CONFIG="/etc/wolf/cfg/config.toml"
+
+              APPS=$(${tomlBin} get "$CONFIG" apps | ${jqBin} length)
+              TMP_FOLDER="$(mktemp -d -t -p /tmp/icedos wolf-extra-volumes-XXXXXXX-0 | xargs echo)/"
+
+              if [[ ! -e "$CONFIG" ]]; then
+                touch "$CONFIG"
+              fi
+
+              cd "$TMP_FOLDER"
+
+              for i in $(seq 0 $((APPS - 1))); do
+                ${tomlBin} set "$CONFIG" apps[$i].runner.mounts '[${concatStringsSep "," (map toJSON volumes)}]' > tmp
+              done
+
+              sed -i 's/^\s*mounts = "\[\(.*\)\]"$/mounts = [\1]/; /^\s*mounts = / s/\\"/\"/g' tmp
+              cat tmp > "$CONFIG"
+              rm -rf "$TMP_FOLDER"
+            '';
         }
       )
     ];
