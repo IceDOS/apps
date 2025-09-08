efb79cd1dc5fad084d51b394efc24ddd35deb382
IceDBorn <git.outsider841@simplelogin.fr>
2025-09-08 22:19:10 +0300

    chore(shadps4): change to master unstable as custom build default

--- modules/shadps4/icedos.nix
+++ modules/shadps4/icedos.nix
@@ -8,10 +8,10 @@
     in
     {
       enable = mkBoolOption { default = false; };
-      hash = mkStrOption { default = "sha256-jLCWwNjRYneuYQg0hQVHuVW4e+rWaoYWj9kAOzwqdmw="; };
-      owner = mkStrOption { default = "diegolix29"; };
+      hash = mkStrOption { default = "sha256-mxLv1IUHDQWUOvoapaueZO76+bCNsZK5IHuJ6rjF0aE="; };
+      owner = mkStrOption { default = "shadps4-emu"; };
       repo = mkStrOption { default = "shadps4"; };
-      tag = mkStrOption { default = "BBFork-shadPS4-2025-09-07-9468c80"; };
+      tag = mkStrOption { default = "Pre-release-shadPS4-2025-09-08-133f4b9"; };
     };
 
   outputs.nixosModules =
@@ -32,36 +32,31 @@
           environment.systemPackages =
             if (customBuild.enable) then
               [
-                (
+                (shadps4.overrideAttrs (
+                  super:
                   let
-                    inherit (builtins) substring;
+                    inherit (customBuild)
+                      hash
+                      owner
+                      repo
+                      tag
+                      ;
                   in
-                  shadps4.overrideAttrs (
-                    super:
-                    let
-                      inherit (customBuild)
+                  {
+                    src = fetchFromGitHub {
+                      inherit (super.src) fetchSubmodules;
+
+                      inherit
                         hash
                         owner
                         repo
                         tag
                         ;
-                    in
-                    {
-                      src = fetchFromGitHub {
-                        inherit (super.src) fetchSubmodules;
+                    };
 
-                        inherit
-                          hash
-                          owner
-                          repo
-                          tag
-                          ;
-                      };
-
-                      version = substring 26 33 tag;
-                    }
-                  )
-                )
+                    version = tag;
+                  }
+                ))
               ]
             else
               [ shadps4 ];
