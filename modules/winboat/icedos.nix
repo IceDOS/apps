6a9132aa78f1b2410071fb7a07cdc6df045b92b7
IceDBorn <git.outsider841@simplelogin.fr>
2025-09-29 12:52:42 +0300

    rework(winboat): update module for flake changes

--- modules/winboat/icedos.nix
+++ modules/winboat/icedos.nix
@@ -11,7 +11,7 @@
   outputs.nixosModules =
     { inputs, ... }:
     [
-      inputs.winboat.nixosModules."x86_64-linux".default
+      { environment.systemPackages = [ inputs.winboat.packages.x86_64-linux.winboat ]; }
 
       (
         {
@@ -21,7 +21,13 @@
           ...
         }:
         {
-          services.winboat.enable = true;
+          boot.kernelModules = [ "iptable_nat" ];
+
+          environment.systemPackages = with pkgs; [
+            freerdp3
+            docker-compose
+            iptables
+          ];
 
           icedos.applications.toolset.commands = [
             (
@@ -42,10 +48,6 @@
             )
           ];
 
-          users.users = lib.mapAttrs (user: _: {
-            extraGroups = [ "docker" ];
-          }) config.icedos.users;
-
           systemd.services.docker.serviceConfig.ExecStartPost =
             let
               cfg = config.icedos;
@@ -55,6 +57,13 @@
             mkIf (!cfg.applications.winboat.autostart) ''
               (${docker}/bin/docker stop WinBoat || exit 0)
             '';
+
+          users.users = lib.mapAttrs (user: _: {
+            extraGroups = [ "docker" ];
+          }) config.icedos.users;
+
+          virtualisation.docker.enable = true;
+          virtualisation.libvirtd.enable = true;
         }
       )
     ];
