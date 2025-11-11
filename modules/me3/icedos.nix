deb88a51117948c83f8038c053357844865fabcd
IceDBorn <git.outsider841@simplelogin.fr>
2025-11-11 20:39:09 +0200

    feat(me3): add profiles depending on other profiles

--- modules/me3/icedos.nix
+++ modules/me3/icedos.nix
@@ -1,10 +1,15 @@
 { icedosLib, ... }:
 
 {
-  options.icedos.applications.me3.profiles = icedosLib.mkSubmoduleListOption { default = [ ]; } {
-    config = icedosLib.mkStrOption { default = ""; };
-    name = icedosLib.mkStrOption { default = ""; };
-  };
+  options.icedos.applications.me3.profiles =
+    let
+      inherit (icedosLib) mkSubmoduleListOption mkStrOption mkStrListOption;
+    in
+    mkSubmoduleListOption { default = [ ]; } {
+      config = mkStrOption { default = ""; };
+      dependencies = mkStrListOption { default = [ ]; };
+      name = mkStrOption { default = ""; };
+    };
 
   outputs.nixosModules =
     { ... }:
@@ -39,7 +44,17 @@
             mapAttrs (user: _: {
               home.file =
                 let
-                  inherit (builtins) foldl';
+                  inherit (icedosLib) abortIf;
+
+                  inherit (lib)
+                    concatMapStringsSep
+                    filter
+                    foldl'
+                    head
+                    length
+                    ;
+
+                  inherit (config.icedos.applications.me3) profiles;
                   inherit (pkgs) me3;
                   homeWindowsBinPath = ".local/share/me3/windows-bin";
                   windowsBinPath = "${me3}/share/me3/windows-bin";
@@ -49,9 +64,34 @@
                   "${homeWindowsBinPath}/me3_mod_host.dll".source = "${windowsBinPath}/me3_mod_host.dll";
                 }
                 // foldl' (acc: atrrset: acc // atrrset) { } (
-                  map (profile: {
-                    ".config/me3/profiles/${profile.name}.me3".text = profile.config;
-                  }) cfg.applications.me3.profiles
+                  map (
+                    profile:
+                    let
+                      filterProfiles = name: profiles: (filter (profile: profile.name == name) profiles);
+                      duplicateProfiles = length (filterProfiles profile.name cfg.applications.me3.profiles);
+                      getProfileByName = name: head (filter (p: p.name == name) profiles);
+
+                      dependencies = concatMapStringsSep "\n" (
+                        depName: (getProfileByName depName).config
+                      ) profile.dependencies;
+
+                      config =
+                        if
+                          (abortIf (duplicateProfiles > 1)
+                            ''${toString duplicateProfiles} me3 profiles named "${profile.name}" detected - profile names have to be unique!''
+                          )
+                        then
+                          profile.config
+                        else
+                          "";
+                    in
+                    {
+                      ".config/me3/profiles/${profile.name}.me3".text = ''
+                        ${dependencies}
+                        ${config}
+                      '';
+                    }
+                  ) profiles
                 );
             }) (listToAttrs (icedosLib.getNormalUsers { inherit (config.users) users; }));
         }
