32025a3a205f9c93ac6e1bd976e6f1a39fc121dc
jim3692 <31220180+jim3692@users.noreply.github.com>
2025-11-14 04:54:46 +0200

    celluloid: Fetch shader from Gist (#11)
    * celluloid: Fetch shader from Gist
    
    * refactor(celluloid): cleanup
    
    ---------
    
    Co-authored-by: IceDBorn <git.outsider841@simplelogin.fr>

--- modules/celluloid/icedos.nix
+++ modules/celluloid/icedos.nix
@@ -1,8 +1,23 @@
-{ ... }:
+{ lib, ... }:
 
+let
+  inherit (builtins) fetchurl toFile;
+  inherit (lib) importJSON;
+
+  shaderFileName = "FSR.glsl";
+  shaderGist = importJSON (fetchurl "https://api.github.com/gists/82219c545228d70c5604f865ce0b0ce5");
+  shader = shaderGist.files.${shaderFileName}.content;
+in
 {
+  inputs = {
+    celluloid-shader = {
+      url = "path://${toFile shaderFileName shader}";
+      flake = false;
+    };
+  };
+
   outputs.nixosModules =
-    { ... }:
+    { inputs, ... }:
     [
       (
         {
@@ -11,6 +26,10 @@
           config,
           ...
         }:
+
+        let
+          inherit (config.icedos) users;
+        in
         {
           environment.systemPackages = with pkgs; [
             (writeShellScriptBin "celluloid-hdr" "ENABLE_HDR_WSI=1 celluloid --mpv-profile=HDR $@")
@@ -18,10 +37,8 @@
           ];
 
           home-manager.users = lib.mapAttrs (user: _: {
-            home.file.".config/celluloid" = {
-              source = ./config;
-              recursive = true;
-            };
+            home.file.".config/celluloid/celluloid.conf".source = ./celluloid.conf;
+            home.file.".config/celluloid/shaders/FSR.glsl".source = inputs.celluloid-shader;
 
             dconf.settings = {
               "io/github/celluloid-player/celluloid" = {
@@ -36,7 +53,7 @@
                 always-append-to-playlist = true;
               };
             };
-          }) config.icedos.users;
+          }) users;
         }
       )
     ];
