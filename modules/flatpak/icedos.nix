b44b5b1f45b9d4bb36af93f1092a371b305dce3e
IceDBorn <git.outsider841@simplelogin.fr>
2025-11-23 04:11:18 +0200

    rework(flatpak): use nix-flatpak

--- modules/flatpak/icedos.nix
+++ modules/flatpak/icedos.nix
@@ -1,36 +1,72 @@
-{ ... }:
+{ icedosLib, lib, ... }:
 
 {
+  inputs.nix-flatpak.url = "github:gmodena/nix-flatpak/";
+
+  options.icedos.applications.flatpak.packages =
+    let
+      inherit (lib) readFile;
+      inherit (icedosLib) mkStrListOption;
+      inherit ((fromTOML (readFile ./config.toml)).icedos.applications.flatpak) packages;
+    in
+    mkStrListOption { default = packages; };
+
   outputs.nixosModules =
-    { ... }:
+    { inputs, ... }:
     [
       (
-        { pkgs, ... }:
+        {
+          config,
+          lib,
+          ...
+        }:
         let
-          inherit (pkgs) flatpak writeShellScriptBin;
+          inherit (config.icedos) applications users;
+          inherit (applications.flatpak) packages;
+
+          inherit (lib)
+            elemAt
+            length
+            mapAttrs
+            mkOptionDefault
+            splitString
+            ;
         in
         {
-          icedos.applications.toolset.commands = [
-            (
-              let
-                command = "flatpak-init";
-                flatpakBin = "${flatpak}/bin/flatpak";
-              in
-              {
-                inherit command;
-
-                bin = "${writeShellScriptBin command ''
-                  ${flatpakBin} remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-                  ${flatpakBin} install flathub com.github.tchx84.Flatseal
-                  ${flatpakBin} install flathub io.github.kolunmi.Bazaar
-                ''}/bin/${command}";
-
-                help = "add flathub as a flatpak repo and install flatpak helper tools";
-              }
-            )
-          ];
-
           services.flatpak.enable = true;
+
+          home-manager.users = mapAttrs (user: _: {
+            imports = [ inputs.nix-flatpak.homeManagerModules.nix-flatpak ];
+
+            services.flatpak = {
+              packages = map (
+                package:
+                let
+                  packageParts = splitString ":" package;
+
+                  package' =
+                    if (length packageParts > 1) then
+                      {
+                        appId = elemAt packageParts 1;
+                        origin = elemAt packageParts 0;
+                      }
+                    else
+                      {
+                        appId = package;
+                        origin = "flathub";
+                      };
+                in
+                package'
+              ) packages;
+
+              remotes = mkOptionDefault [
+                {
+                  name = "flathub-beta";
+                  location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+                }
+              ];
+            };
+          }) users;
         }
       )
     ];
