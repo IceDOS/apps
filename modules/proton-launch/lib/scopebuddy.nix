cb9339a6624468a5fd55bca5e21ae8ce932b973c
IceDBorn <git.outsider841@simplelogin.fr>
2025-12-19 19:04:34 +0200

    update(scopebuddy): 1.3.0 -> 1.3.1

--- modules/proton-launch/lib/scopebuddy.nix
+++ modules/proton-launch/lib/scopebuddy.nix
@@ -5,13 +5,13 @@
 
 stdenvNoCC.mkDerivation (final: {
   name = "scopebuddy";
-  version = "1.3.0";
+  version = "1.3.1";
 
   src = fetchFromGitHub {
     owner = "HikariKnight";
     repo = "ScopeBuddy";
     tag = final.version;
-    hash = "sha256-tJkIt1io4M9X4Lzs/mm4K5xd7ZUCMnXVCeWv4huccx4=";
+    hash = "sha256-mTDg36TQd0Q3CsNCfOxM55JhyXYHEcV41NWsiaUB0+4=";
   };
 
   installPhase = ''
