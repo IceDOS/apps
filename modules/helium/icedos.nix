1fe6264aad387baa07ac1ab0eb8b509de4628f65
IceDBorn <git.outsider841@simplelogin.fr>
2025-12-23 14:01:19 +0200

    feat: add helium chromium browser

---
+++ modules/helium/icedos.nix
@@ -0,0 +1,99 @@
+{ icedosLib, ... }:
+
+{
+  options.icedos.applications.helium =
+    let
+      inherit (icedosLib)
+        mkStrListOption
+        mkStrOption
+        mkSubmoduleListOption
+        ;
+    in
+    {
+      profiles = mkSubmoduleListOption { default = [ ]; } {
+        exec = mkStrOption { };
+        icon = mkStrOption { default = ""; };
+        name = mkStrOption { default = ""; };
+        sites = mkStrListOption { default = [ ]; };
+      };
+    };
+
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        let
+          inherit (config.icedos.applications) defaultBrowser;
+          inherit (pkgs.nur.repos.Ev357) helium;
+          inherit (lib) mkIf;
+        in
+        {
+          environment = {
+            sessionVariables.DEFAULT_BROWSER = mkIf (defaultBrowser == "helium.desktop") "${helium}/bin/helium";
+            systemPackages = [ helium ];
+          };
+        }
+      )
+
+      # Profiles
+      (
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+
+        let
+          inherit (lib)
+            listToAttrs
+            mapAttrs
+            ;
+
+          inherit (config.icedos) applications users;
+          inherit (applications.helium) profiles;
+        in
+        {
+          environment.systemPackages = map (
+            profile:
+            pkgs.writeShellScriptBin profile.exec ''
+              helium --profile-directory="${profile.exec}" ${toString profile.sites}
+            ''
+          ) profiles;
+
+          home-manager.users = mapAttrs (user: _: {
+            xdg.desktopEntries = listToAttrs (
+              map (profile: {
+                name = profile.exec;
+
+                value = {
+                  exec = profile.exec;
+                  icon = profile.icon;
+                  name = profile.name;
+                  terminal = false;
+                  type = "Application";
+                };
+              }) profiles
+            );
+          }) users;
+        }
+      )
+    ];
+
+  meta = {
+    name = "helium";
+
+    dependencies = [
+      {
+        url = "github:icedos/providers";
+        modules = [ "nur" ];
+      }
+    ];
+  };
+}
