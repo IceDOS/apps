175a0f8e3557cdf2b72d574ce257be820596aaf3
IceDBorn <51162078+IceDBorn@users.noreply.github.com>
2026-01-31 18:44:53 +0200

    rework(toolset): remove base and rebuild, part of core (#21)

--- modules/toolset/icedos.nix
+++ modules/toolset/icedos.nix
@@ -16,21 +16,15 @@
     [
       (
         {
-          config,
           lib,
-          pkgs,
           ...
         }:
 
         let
           inherit (lib)
             attrNames
-            concatMapStrings
             filterAttrs
-            sort
             ;
-
-          cfg = config.icedos;
 
           getModules =
             path:
@@ -39,42 +33,7 @@
             );
         in
         {
-          imports = getModules (./modules);
-
-          environment.systemPackages = [
-            (
-              let
-                purpleString = string: ''''${PURPLE}${string}''${NC}'';
-              in
-              pkgs.writeShellScriptBin "icedos" ''
-                PURPLE='\033[0;35m'
-                NC='\033[0m'
-
-                if [[ "$1" == "" || "$1" == "help" ]]; then
-                  echo "Available commands:"
-
-                  ${concatMapStrings (tool: ''
-                    echo -e "> ${purpleString tool.command}: ${tool.help} "
-                  '') (sort (a: b: a.command < b.command) (cfg.applications.toolset.commands))}
-
-                  exit 0
-                fi
-
-                case "$1" in
-                  ${concatMapStrings (tool: ''
-                    ${tool.command})
-                      shift
-                      exec ${tool.bin} "$@"
-                      ;;
-                  '') cfg.applications.toolset.commands}
-                  *|-*|--*)
-                    echo "Unknown arg: $1" >&2
-                    exit 1
-                    ;;
-                esac
-              ''
-            )
-          ];
+          imports = getModules ./modules;
         }
       )
     ];
