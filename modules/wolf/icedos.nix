999882adef89eeb365b090fa029176763224273e
IceDBorn <git.outsider841@simplelogin.fr>
2026-04-08 23:50:20 +0300

    fix(wolf): startup errors because of missing targets

--- modules/wolf/icedos.nix
+++ modules/wolf/icedos.nix
@@ -7,20 +7,20 @@
       inherit (icedosLib) mkStrOption mkStrListOption;
 
       inherit ((fromTOML (readFile ./config.toml)).icedos.applications.wolf)
-        branch
         extraEnvironmentFlags
         extraOptions
         extraPackages
         extraVolumes
+        image
         stateFolder
         ;
     in
     {
-      branch = mkStrOption { default = branch; };
       extraEnvironmentFlags = mkOption { default = extraEnvironmentFlags; };
       extraOptions = mkStrListOption { default = extraOptions; };
       extraPackages = mkStrListOption { default = extraPackages; };
       extraVolumes = mkStrListOption { default = extraVolumes; };
+      image = mkStrOption { default = image; };
       stateFolder = mkStrOption { default = stateFolder; };
     };
 
@@ -30,23 +30,64 @@
       (
         {
           config,
+          lib,
           pkgs,
           ...
         }:
+
         let
-          wolf = config.icedos.applications.wolf;
+          inherit (config) icedos fileSystems;
+          inherit (icedos.applications) wolf;
+
+          inherit (lib)
+            any
+            attrNames
+            concatStringsSep
+            filter
+            hasPrefix
+            head
+            removePrefix
+            replaceStrings
+            splitString
+            ;
+
+          inherit (wolf) extraVolumes stateFolder;
+
+          # Dynamic mount dependencies based on configured volumes
+          hostPaths = [ stateFolder ] ++ map (v: head (splitString ":" v)) extraVolumes;
+
+          mountPoints = filter (mp: mp != "/" && any (hp: hasPrefix (mp + "/") hp || mp == hp) hostPaths) (
+            attrNames fileSystems
+          );
+
+          # Convert filesystem path to systemd mount unit name
+          # Hyphens in path components must be escaped as \x2d to match systemd's escaping
+          pathToMountUnit =
+            path:
+            let
+              components = splitString "/" (removePrefix "/" path);
+              escaped = map (c: replaceStrings [ "-" ] [ "\\x2d" ] c) components;
+            in
+            "${concatStringsSep "-" escaped}.mount";
+
+          requiredMounts = map pathToMountUnit mountPoints;
         in
         {
           virtualisation = {
             oci-containers =
               let
-                inherit (wolf) branch stateFolder;
+                inherit (wolf)
+                  extraEnvironmentFlags
+                  extraOptions
+                  image
+                  stateFolder
+                  ;
               in
               {
                 backend = "docker";
 
                 containers.wolf = {
-                  image = "ghcr.io/games-on-whales/wolf:${branch}";
+                  image = if image != "" then image else "ghcr.io/games-on-whales/wolf";
 
                   extraOptions = [
                     "--pull=always"
@@ -56,13 +97,13 @@
                     "--device=/dev/uinput"
                     "--device=/dev/uhid"
                   ]
-                  ++ wolf.extraOptions;
+                  ++ extraOptions;
 
                   environment = {
                     HOST_APPS_STATE_FOLDER = stateFolder;
                     XDG_RUNTIME_DIR = "/tmp/sockets";
                   }
-                  // wolf.extraEnvironmentFlags;
+                  // extraEnvironmentFlags;
 
                   volumes = [
                     "${stateFolder}:${stateFolder}"
@@ -77,9 +118,9 @@
 
           systemd.services.docker-wolf.preStart =
             let
-              inherit (builtins) attrNames toJSON readDir;
-              inherit (lib) concatStringsSep flatten;
-              inherit (pkgs) jq toml-cli;
+              inherit (builtins) attrNames readDir toJSON;
+              inherit (icedosLib) getNormalUsers;
+              inherit (lib) concatStringsSep flatten makeBinPath;
               inherit (wolf) extraPackages extraVolumes stateFolder;
 
               defaultVolumes = [
@@ -98,18 +139,39 @@
                 ) extraPackages
               );
 
-              jqBin = "${jq}/bin/jq";
-              tomlBin = "${toml-cli}/bin/toml";
-
-              userPackagesVolumes = map (user: "${user.value.home}/.nix-profile/bin:/host-apps/${user.name}:ro") (
-                icedosLib.getNormalUsers { inherit (config.users) users; }
-              );
+              userPackagesVolumes =
+                map
+                  (
+                    user:
+                    let
+                      inherit (user) value name;
+                      inherit (value) home;
+                    in
+                    "${home}/.nix-profile/bin:/host-apps/${name}:ro"
+                  )
+                  (getNormalUsers {
+                    inherit (config.users) users;
+                  });
 
               volumes = defaultVolumes ++ extraPackagesVolumes ++ extraVolumes ++ userPackagesVolumes;
             in
             ''
+              PATH=${
+                with pkgs;
+                makeBinPath [
+                  toml-cli
+                  jq
+                ]
+              }:$PATH
+
+              # Wait for DNS to become available
+              for i in $(seq 1 30); do
+                getent hosts ghcr.io > /dev/null 2>&1 && break
+                sleep 1
+              done
+
               CONFIG="${stateFolder}/cfg/config.toml"
-              PROFILES=$(${tomlBin} get "$CONFIG" profiles | ${jqBin} length)
+              PROFILES=$(toml get "$CONFIG" profiles | jq length)
               TMP_FOLDER="$(mktemp -d -t -p /tmp/icedos wolf-extra-volumes-XXXXXXX-0 | xargs echo)/"
 
               if [[ ! -e "$CONFIG" ]]; then
@@ -120,10 +182,10 @@
               cat "$CONFIG" > tmp
 
               for i in $(seq 0 $((PROFILES - 1))); do
-                APPS=$(${tomlBin} get "$CONFIG" profiles[$i].apps | ${jqBin} length)
+                APPS=$(toml get "$CONFIG" profiles[$i].apps | jq length)
 
                 for x in $(seq 0 $((APPS - 1))); do
-                  ${tomlBin} set tmp profiles[$i].apps[$x].runner.mounts '[${concatStringsSep "," (map toJSON volumes)}]' > tmp2
+                  toml set tmp profiles[$i].apps[$x].runner.mounts '[${concatStringsSep "," (map toJSON volumes)}]' > tmp2
                   sed -i 's/^\s*mounts = "\[\(.*\)\]"$/mounts = [\1]/; /^\s*mounts = / s/\\"/\"/g' tmp2
                   cp tmp2 tmp
                 done
@@ -134,6 +196,9 @@
             '';
 
           systemd.services.docker-wolf.serviceConfig.RestartSec = 10;
+          systemd.services.docker-wolf.after = [ "network-online.target" ] ++ requiredMounts;
+          systemd.services.docker-wolf.wants = [ "network-online.target" ];
+          systemd.services.docker-wolf.requires = requiredMounts;
 
           services.udev.extraRules = ''
             # Moonlight / NVIDIA GameStream virtual Xbox controller
