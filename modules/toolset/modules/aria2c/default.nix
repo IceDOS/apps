7392ff0a2d7b997ea3783144ff6334acd791b78c
IceDBorn <git.outsider841@simplelogin.fr>
2026-04-22 22:41:55 +0300

    feat(toolset): add transmission to download torrents

--- modules/toolset/modules/aria2c/default.nix
+++ modules/toolset/modules/aria2c/default.nix
@@ -9,6 +9,17 @@
       command = "download";
 
       script = ''
+        if [[ "$1" == "--torrent" ]]; then
+          if [[ "$2" == "" ]]; then
+            echo "error: specify torrent url/magnet/file as an argument"
+            echo "usage: icedos download --torrent [URI | MAGNET | TORRENT_FILE] [DOWNLOAD_DIR]"
+            exit 1
+          fi
+
+          "${pkgs.transmission_4}/bin/transmission-cli" -B -D -ep -U -w "''${3:-.}" "$2"
+          exit $?
+        fi
+
         if [[ "$1" == "" ]]; then
           echo "error: specify url as an argument"
           echo "usage: icedos download [OPTIONS] [URI | MAGNET | TORRENT_FILE | METALINK_FILE]..."
@@ -19,7 +30,7 @@
         "${pkgs.aria2}/bin/aria2c" -j 16 -s 16 "$@"
       '';
 
-      help = "download provided url using aria2c utilizing 16 connections";
+      help = "download provided url using aria2c or transmission, aiming max speed";
     }
   ];
 }
