10605cb746b52166a8e68ebaa8f128d21974a5df
IceDBorn <git.outsider841@simplelogin.fr>
2026-05-02 17:12:32 +0300

    feat(btop): add polkit based elevated btop desktop entry

--- modules/btop/icedos.nix
+++ modules/btop/icedos.nix
@@ -8,11 +8,20 @@
         {
           config,
           lib,
+          pkgs,
           ...
         }:
 
         let
           inherit (lib) mkIf;
+
+          btop-sudo = pkgs.writeShellScriptBin "btop-sudo" ''
+            CONFIG_DIR="''${XDG_CONFIG_HOME:-$HOME/.config}/btop"
+            exec /run/wrappers/bin/pkexec ${pkgs.btop}/bin/btop \
+              -c "$CONFIG_DIR/btop.conf" \
+              --themes-dir "$CONFIG_DIR/themes" \
+              "$@"
+          '';
         in
         {
           home-manager.sharedModules = [
@@ -22,6 +31,24 @@
               xdg.configFile."btop/btop.conf" = mkIf (!(config.stylix.enable or false)) {
                 source = ./btop.conf;
                 force = true;
+              };
+
+              xdg.desktopEntries.btop-sudo = {
+                name = "sudo btop++";
+                genericName = "System Monitor";
+                comment = "Resource monitor that shows usage and stats for processor, memory, disks, network and processes";
+                icon = "btop";
+                exec = "${btop-sudo}/bin/btop-sudo";
+                terminal = true;
+                type = "Application";
+
+                categories = [
+                  "System"
+                  "Monitor"
+                  "ConsoleOnly"
+                ];
+
+                settings.Keywords = "system;process;task;root;sudo";
               };
             }
           ];
