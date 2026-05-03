5dc1093f17c2cda2ed164cf31a191997822d7288
IceDBorn <git.outsider841@simplelogin.fr>
2026-05-03 18:28:28 +0300

    rework(toolset): add desktop entries for logout/reboot

--- modules/toolset/modules/reboot/default.nix
+++ modules/toolset/modules/reboot/default.nix
@@ -1,5 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  rebootBin = pkgs.writeShellScriptBin "icedos-reboot" ''
+    exec /run/wrappers/bin/pkexec ${pkgs.systemd}/bin/systemctl reboot -i
+  '';
+
+  rebootUefiBin = pkgs.writeShellScriptBin "icedos-reboot-uefi" ''
+    exec /run/wrappers/bin/pkexec ${pkgs.systemd}/bin/systemctl reboot --firmware-setup -i
+  '';
+in
 {
   icedos.applications.toolset.commands = [
     {
@@ -8,10 +17,10 @@
       script = ''
         case "$1" in
           "")
-            systemctl reboot -i
+            systemctl reboot -i || sudo systemctl reboot -i
             ;;
           uefi)
-            systemctl reboot --firmware-setup -i
+            systemctl reboot --firmware-setup -i || sudo systemctl reboot --firmware-setup -i
             ;;
           *)
             die "unknown arg: $1"
@@ -20,6 +29,40 @@
       '';
 
       help = "reboot ignoring inhibitors and users, uefi supported by appending it as an argument";
+    }
+  ];
+
+  home-manager.sharedModules = [
+    {
+      xdg.desktopEntries.icedos-reboot = {
+        name = "Reboot";
+        genericName = "Restart the system";
+        comment = "Reboot the system, ignoring inhibitors and other logged-in users";
+        icon = "system-reboot";
+        exec = "${rebootBin}/bin/icedos-reboot";
+        terminal = false;
+        type = "Application";
+        categories = [
+          "System"
+          "Settings"
+        ];
+        settings.Keywords = "reboot;restart;shutdown;";
+      };
+
+      xdg.desktopEntries.icedos-reboot-uefi = {
+        name = "Reboot to UEFI";
+        genericName = "Restart into firmware setup";
+        comment = "Reboot the system into the UEFI firmware setup screen";
+        icon = "system-reboot";
+        exec = "${rebootUefiBin}/bin/icedos-reboot-uefi";
+        terminal = false;
+        type = "Application";
+        categories = [
+          "System"
+          "Settings"
+        ];
+        settings.Keywords = "reboot;restart;uefi;firmware;bios;";
+      };
     }
   ];
 }
