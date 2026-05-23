d7cc5993640f04b671335124ce580c6ca1aaf283
CrazyStevenz <github.ekta@aleeas.com>
2026-05-23 18:14:36 +0300

    Allow selecting which aagl launchers you want to enable

--- modules/aagl/icedos.nix
+++ modules/aagl/icedos.nix
@@ -10,11 +10,29 @@
     { inputs, ... }:
     [
       inputs.aagl.nixosModules.default
+      (
+        {
+          lib,
+          ...
+        }:
+        let
+          inherit (lib) readFile;
 
-      {
-        nix.settings = inputs.aagl.nixConfig; # Set up Cachix
-        programs.anime-game-launcher.enable = true; # Adds launcher and /etc/hosts rules
-      }
+          inherit ((fromTOML (readFile ./config.toml)).icedos.applications.aagl)
+            launchers
+            ;
+        in
+        {
+          nix.settings = inputs.aagl.nixConfig; # Set up Cachix
+
+          programs = lib.listToAttrs (
+            map (game: {
+              name = game;
+              value.enable = true;
+            }) launchers # Adds launchers and /etc/hosts rules
+          );
+        }
+      )
     ];
 
   meta.name = "aagl";
