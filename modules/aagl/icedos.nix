d83263f6404a53b890dd8b54316b8fdc7575b8b9
Stefanos <github.ekta@aleeas.com>
2026-05-23 18:31:05 +0300

    Allow selecting which aagl launchers you want to enable (#26)
    * Allow selecting which aagl launchers you want to enable
    
    * Fix not using options from consumer's config.toml
    
    ---------
    
    Co-authored-by: IceDBorn <51162078+IceDBorn@users.noreply.github.com>

--- modules/aagl/icedos.nix
+++ modules/aagl/icedos.nix
@@ -1,6 +1,22 @@
-{ ... }:
+{ lib, icedosLib, ... }:
 
 {
+  options.icedos.applications.aagl =
+    let
+      inherit (lib) readFile;
+
+      inherit (icedosLib)
+        mkStrListOption
+        ;
+
+      inherit ((fromTOML (readFile ./config.toml)).icedos.applications.aagl)
+        launchers
+        ;
+    in
+    {
+      launchers = mkStrListOption { default = launchers; };
+    };
+
   inputs.aagl = {
     url = "github:ezKEa/aagl-gtk-on-nix";
     inputs.nixpkgs.follows = "nixpkgs";
@@ -10,11 +26,25 @@
     { inputs, ... }:
     [
       inputs.aagl.nixosModules.default
+      (
+        {
+          config,
+          ...
+        }:
+        let
+          inherit (config.icedos.applications.aagl) launchers;
+        in
+        {
+          nix.settings = inputs.aagl.nixConfig; # Set up Cachix
 
-      {
-        nix.settings = inputs.aagl.nixConfig; # Set up Cachix
-        programs.anime-game-launcher.enable = true; # Adds launcher and /etc/hosts rules
-      }
+          programs = lib.listToAttrs (
+            map (game: {
+              name = game;
+              value.enable = true;
+            }) launchers # Adds launchers and /etc/hosts rules
+          );
+        }
+      )
     ];
 
   meta.name = "aagl";
